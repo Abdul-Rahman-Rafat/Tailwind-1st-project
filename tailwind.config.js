b66b4cb5cdc083265a1ab17e12383cabc6ab884a
Abdulrahman Rafat Ahmed <140813224+Abdul-Rahman-Rafat@users.noreply.github.com>
2026-01-10 07:37:18 +0200

    carousel - features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        planeMove: {
+          '0%': { transform: 'skew(0deg)' },
+          '50%': { transform: 'skew(15deg)' },
+          '100%': { transform: 'skew(0deg)' },
+        },
+      },
+      animation: {
+        plane: 'planeMove 3s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
